dc5b01e693c399c869ddcf9380526cc0e06d9d4d
Adam Alexander <adam.alexander@rightscale.com>
2017-09-10 18:32:59 -0400

    Modify template for App Service Plan

--- sample.tf
+++ sample.tf
@@ -1,9 +1,19 @@
-provider "aws" {
-  region  = "us-east-1"
+provider "azurerm" {
   version = "~> 0.1"
 }
 
-resource "aws_instance" "sample" {
-  ami           = "ami-2757f631"
-  instance_type = "m3.medium"
+resource "azurerm_resource_group" "ss_tf_rg" {
+  name     = "ss_tf_rg"
+  location = "Central US"
+}
+
+resource "azurerm_app_service_plan" "ss_tf_asp" {
+  name                = "ss_tf_asp"
+  location            = "Central US"
+  resource_group_name = "${azurerm_resource_group.ss_tf_rg.name}"
+
+  sku {
+    tier = "Free"
+    size = "F1"
+  }
 }

d3c9828011323a8b1825e328c29f10b0f94aa8e0
Adam Alexander <adam.alexander@rightscale.com>
2017-09-10 19:04:35 -0400

    Add some more resources

--- sample.tf
+++ sample.tf
@@ -17,3 +17,30 @@ resource "azurerm_app_service_plan" "ss_tf_asp" {
     size = "F1"
   }
 }
+
+resource "azurerm_application_insights" "ss_tf_ai" {
+  name                = "ss_tf_ai"
+  location            = "Central US"
+  resource_group_name = "${azurerm_resource_group.ss_tf_rg.name}"
+  application_type    = "Web"
+}
+
+resource "azurerm_storage_account" "ss_tf_sa" {
+  name                = "ss_tf_sa"
+  resource_group_name = "${azurerm_resource_group.ss_tf_rg.name}"
+  location            = "${azurerm_resource_group.ss_tf_rg.location}"
+  account_type        = "Standard_GRS"
+}
+
+resource "azurerm_container_registry" "ss_tf_cr" {
+  name                = "ss_tf_cr"
+  resource_group_name = "${azurerm_resource_group.ss_tf_rg.name}"
+  location            = "${azurerm_resource_group.ss_tf_rg.location}"
+  admin_enabled       = true
+  sku                 = "Basic"
+
+  storage_account {
+    name       = "${azurerm_storage_account.ss_tf_sa.name}"
+    access_key = "${azurerm_storage_account.ss_tf_sa.primary_access_key}"
+  }
+}

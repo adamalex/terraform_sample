30182e3f7c42dff0a069e6fce2dd849d264f1ab7
Adam Alexander <adam.alexander@rightscale.com>
2017-09-10 19:12:40 -0400

    Modify some names

--- sample.tf
+++ sample.tf
@@ -2,15 +2,15 @@ provider "azurerm" {
   version = "~> 0.1"
 }
 
-resource "azurerm_resource_group" "ss_tf_rg" {
-  name     = "ss_tf_rg"
+resource "azurerm_resource_group" "sstfrg" {
+  name     = "sstfrg"
   location = "Central US"
 }
 
-resource "azurerm_app_service_plan" "ss_tf_asp" {
-  name                = "ss_tf_asp"
-  location            = "Central US"
-  resource_group_name = "${azurerm_resource_group.ss_tf_rg.name}"
+resource "azurerm_app_service_plan" "sstfasp" {
+  name                = "sstfasp"
+  resource_group_name = "${azurerm_resource_group.sstfrg.name}"
+  location            = "${azurerm_resource_group.sstfrg.location}"
 
   sku {
     tier = "Free"
@@ -18,29 +18,29 @@ resource "azurerm_app_service_plan" "ss_tf_asp" {
   }
 }
 
-resource "azurerm_application_insights" "ss_tf_ai" {
-  name                = "ss_tf_ai"
-  location            = "Central US"
-  resource_group_name = "${azurerm_resource_group.ss_tf_rg.name}"
+resource "azurerm_application_insights" "sstfai" {
+  name                = "sstfai"
+  resource_group_name = "${azurerm_resource_group.sstfrg.name}"
+  location            = "${azurerm_resource_group.sstfrg.location}"
   application_type    = "Web"
 }
 
-resource "azurerm_storage_account" "ss_tf_sa" {
-  name                = "ss_tf_sa"
-  resource_group_name = "${azurerm_resource_group.ss_tf_rg.name}"
-  location            = "${azurerm_resource_group.ss_tf_rg.location}"
+resource "azurerm_storage_account" "sstfsa" {
+  name                = "sstfsa"
+  resource_group_name = "${azurerm_resource_group.sstfrg.name}"
+  location            = "${azurerm_resource_group.sstfrg.location}"
   account_type        = "Standard_GRS"
 }
 
-resource "azurerm_container_registry" "ss_tf_cr" {
-  name                = "ss_tf_cr"
-  resource_group_name = "${azurerm_resource_group.ss_tf_rg.name}"
-  location            = "${azurerm_resource_group.ss_tf_rg.location}"
+resource "azurerm_container_registry" "sstfcr" {
+  name                = "sstfcr"
+  resource_group_name = "${azurerm_resource_group.sstfrg.name}"
+  location            = "${azurerm_resource_group.sstfrg.location}"
   admin_enabled       = true
   sku                 = "Basic"
 
   storage_account {
-    name       = "${azurerm_storage_account.ss_tf_sa.name}"
-    access_key = "${azurerm_storage_account.ss_tf_sa.primary_access_key}"
+    name       = "${azurerm_storage_account.sstfsa.name}"
+    access_key = "${azurerm_storage_account.sstfsa.primary_access_key}"
   }
 }
